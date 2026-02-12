50bebd1e1a9008093f41e885bfa471d1ba9e644b
Hasya <uvulatrachea@gmail.com>
2026-02-13 04:37:51 +0800

    Fix Render env keys (DATABASE_URL)

--- config/database.php
+++ config/database.php
@@ -85,7 +85,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -95,8 +95,9 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
