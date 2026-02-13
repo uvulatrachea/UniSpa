d8d26abe65394517c44a0fcbf401322512e17e31
Hasya <uvulatrachea@gmail.com>
2026-02-13 16:44:57 +0800

    Fix 500 server error on Render: Use DATABASE_URL for PostgreSQL and run migrations/seeders

--- config/database.php
+++ config/database.php
@@ -85,7 +85,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('APP_ENV') === 'production' ? env('DATABASE_URL') : null,
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
