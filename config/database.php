c23f92ab0a1e6a879ab2eb09c1098470e2c6ea6d
Hasya <uvulatrachea@gmail.com>
2026-02-13 17:39:32 +0800

    Hardcode production database credentials in config/database.php

--- config/database.php
+++ config/database.php
@@ -86,16 +86,16 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'dpg-d67eol95pdvs73ectep0-a'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'unispa_db'),
+            'username' => env('DB_USERNAME', 'unispa_db_user'),
+            'password' => env('DB_PASSWORD', 'NylmDujDYUkgjU3jqh3tVmZuaNNCWJNM'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
 
