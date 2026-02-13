b2155bdb769e565cae07a79be90b396ecc254eb1
Hasya <uvulatrachea@gmail.com>
2026-02-13 21:11:26 +0800

    Add Render domain to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -37,6 +37,7 @@ return [
         'http://127.0.0.1:5173',
         'http://localhost:8080',
         'http://127.0.0.1:8080',
+        'https://unispa.onrender.com',
     ],
 
     'allowed_origins_patterns' => [
